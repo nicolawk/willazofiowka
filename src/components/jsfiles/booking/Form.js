1d5f342b591f5d31343ea48c97e89d89176ce03a
wx3r <nefloat@proton.me>
2026-05-14 20:34:56 +0200

    translation fix

--- src/components/jsfiles/booking/Form.js
+++ src/components/jsfiles/booking/Form.js
@@ -184,12 +184,21 @@ const BookingForm = () => {
    return (
     <section className="booking-form-wrap">
       <div className="booking-form-card">
-        <h2 className="bf-title">{t("temporaryTitle")}</h2>
+  <h2 className="bf-title">
+    {t("temporaryTitle", {
+      defaultValue: "Booking temporarily unavailable",
+    })}
+  </h2>
 
-        <p className="bf-subtext">
-          {t("temporaryTechnicalIssues")}
-        </p>
-      </div>
+  <p className="bf-subtext">
+    {t("temporaryTechnicalIssues", {
+      defaultValue:
+        "We are still working on the website and are currently experiencing some technical issues. If you are interested in making a reservation, please contact +48 534 778 477 or anita@polkaapartments.com.",
+    })}
+  </p>
+</div>
+
+
       {false && (
         <>
           <div className="booking-form-card">
